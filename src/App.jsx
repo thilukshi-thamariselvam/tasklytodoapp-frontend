48e654304c4491df3ff656bef4fa2d285b22e0c4
thilukshi-thamariselvam <thilukshi.thamariselvam@dmsswt.com>
2026-03-30 17:16:54 +0530

    implement Today page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
+import TodayPage from './pages/TodayPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
+        <Route index element={<TodayPage />} /> 
         </Route>
       </Routes>
     </BrowserRouter>
