b237d369b480651a146bc99a7feaaf390f5dde7d
thilukshi-thamariselvam <thilukshi.thamariselvam@dmsswt.com>
2026-03-31 11:23:12 +0530

    add updateTask and deleteTask endpoints to taskApi

--- src/api/taskApi.js
+++ src/api/taskApi.js
@@ -3,3 +3,7 @@ import axiosInstance from './axiosInstance';
 export const createTask = (taskData) => axiosInstance.post('/tasks', taskData);
 
 export const getTasks = (userId) => axiosInstance.get(`/tasks?userId=${userId}`);
+
+export const updateTask = (taskId, taskData) => axiosInstance.patch(`/tasks/${taskId}`, taskData);
+
+export const deleteTask = (taskId) => axiosInstance.delete(`/tasks/${taskId}`);
