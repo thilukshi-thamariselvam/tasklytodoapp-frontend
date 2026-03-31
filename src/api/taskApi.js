ff7b313b3d3f20d583b3994ecd9753664ea10897
thilukshi-thamariselvam <thilukshi.thamariselvam@dmsswt.com>
2026-03-31 09:48:50 +0530

    add getTasks endpoint to taskApi

--- src/api/taskApi.js
+++ src/api/taskApi.js
@@ -1,3 +1,5 @@
 import axiosInstance from './axiosInstance';
 
 export const createTask = (taskData) => axiosInstance.post('/tasks', taskData);
+
+export const getTasks = (userId) => axiosInstance.get(`/tasks?userId=${userId}`);
