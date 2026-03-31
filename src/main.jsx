1d074dc83fc40fcf766b1214ac3fdf21a3529144
thilukshi-thamariselvam <thilukshi.thamariselvam@dmsswt.com>
2026-03-31 07:42:17 +0530

    configure Redux store and wrap App in Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux'; // ADD THIS
+import { store } from './store';
 import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from './theme/theme';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <App />
     </ThemeProvider>
+    </Provider>
   </StrictMode>,
 );
