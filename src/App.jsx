0c0eeafaa3380d7c8733b35a3047d86b2f58cbf5
thilukshi-thamariselvam <thilukshi.thamariselvam@dmsswt.com>
2026-03-31 12:45:30 +0530

    wire Inbox page to /inbox route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import TodayPage from './pages/TodayPage';
+import InboxPage from './pages/InboxPage';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
         <Route index element={<TodayPage />} /> 
+        <Route path="inbox" element={<InboxPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
