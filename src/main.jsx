ce9b3287774bfe8525704e8e45fa04c24766fb01
thilukshi-thamariselvam <thilukshi.thamariselvam@dmsswt.com>
2026-03-31 09:50:30 +0530

    add QueryClientProvider to application root

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,25 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
-import { Provider } from 'react-redux'; // ADD THIS
+import { Provider } from 'react-redux';
 import { store } from './store';
 import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from './theme/theme';
 import App from './App.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; 
+
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
     </Provider>
   </StrictMode>,
 );
