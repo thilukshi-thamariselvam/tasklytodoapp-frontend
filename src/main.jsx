9afefbd922e47773b7642e70f4b5351947073fc7
thilukshi-thamariselvam <thilukshi.thamariselvam@dmsswt.com>
2026-03-31 15:19:21 +0530

    add MUI LocalizationProvider with Dayjs adapter for date pickers

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { store } from './store';
 import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from './theme/theme';
 import App from './App.jsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 
 const queryClient = new QueryClient();
@@ -14,12 +16,14 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <App />
-        </ThemeProvider>
-      </QueryClientProvider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+          </ThemeProvider>
+        </QueryClientProvider>
+      </LocalizationProvider>
     </Provider>
   </StrictMode>,
 );
