88706f03bb8a57b7ae0722381510b9f69db397ed
thilukshi-thamariselvam <thilukshi.thamariselvam@dmsswt.com>
2026-04-01 20:12:23 +0530

    add checked prop to task checkbox for proper completion UI

--- src/components/Task/TaskItem.jsx
+++ src/components/Task/TaskItem.jsx
@@ -43,6 +43,7 @@ const TaskItem = ({ task, isHovered, onMouseEnter, onMouseLeave, onEditClick }) 
             }}
         >
             <Checkbox
+                checked={task.status === 'COMPLETED'}
                 icon={
                     <svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="2" />
