e04d032286462ba1712317474f3e5cd67b001a46
thilukshi-thamariselvam <thilukshi.thamariselvam@dmsswt.com>
2026-04-02 14:19:26 +0530

    add priority filter state and actions to Redux store

--- src/store/slices/uiSlice.js
+++ src/store/slices/uiSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   isAddTaskModalOpen: false,
   activeFilterLabelId: null,
+  activeFilterPriority: null,
 };
 
 const uiSlice = createSlice({
@@ -21,8 +22,14 @@ const uiSlice = createSlice({
     clearActiveFilter: (state) => {
       state.activeFilterLabelId = null;
     },
+        setActiveFilterPriority: (state, action) => {
+      state.activeFilterPriority = action.payload;
+    },
+    clearActiveFilterPriority: (state) => {
+      state.activeFilterPriority = null;
+    },
   },
 });
 
-export const { openAddTaskModal, closeAddTaskModal, setActiveFilterLabel, clearActiveFilter } = uiSlice.actions;
+export const { openAddTaskModal, closeAddTaskModal, setActiveFilterLabel, clearActiveFilter, setActiveFilterPriority, clearActiveFilterPriority } = uiSlice.actions;
 export default uiSlice.reducer;
